707c758c3fc8e4e3d4e848b8ab68a214cfe2b4ba
Naur <princessmarielledepalubos2nd@gmail.com>
2025-10-29 21:50:22 +0800

    shes cookinn

---
+++ script.js
@@ -0,0 +1,13 @@
+const time = document.querySelector('.time')
+
+function updateClock() {
+    let now = new Date();
+    let hour = now.getHours();
+    let minute = now.getMinutes();
+
+    hour = hour < 10 ? '0' + hour : hour;
+    minute = minute < 10 ? '0' + minute : minute;
+
+    time.textContent = `${hour}:${minute}`
+}
+setInterval(updateClock, 5000)
