a768a991c45fe3ce1c43d80500f9c75b20a6f4d9
Naur <princessmarielledepalubos2nd@gmail.com>
2025-10-30 00:16:40 +0800

    kawai

--- script.js
+++ script.js
@@ -11,3 +11,16 @@ function updateClock() {
     time.textContent = `${hour}:${minute}`
 }
 setInterval(updateClock, 1000)
+
+const music = document.querySelector('.music-player audio')
+const music_icon = document.querySelector('.music-icon')
+
+music_icon.addEventListener('click', () => {
+    if (music.paused) {
+        music.play()
+        music_icon.textContent = '⏹'
+    } else {
+        music.pause()
+        music_icon.textContent = '▶'
+    }
+})
