360fce832838e0eb5da8386887dad5fa8ccf5064
Naur <princessmarielledepalubos2nd@gmail.com>
2025-10-30 16:39:10 +0800

    mm

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const time = document.querySelector('.time')
 
 function updateClock() {
@@ -12,13 +13,33 @@ function updateClock() {
 }
 setInterval(updateClock, 1000)
 
+
+const images = document.querySelectorAll('.image')
+const active_image = document.querySelector('.image.active')
+const close_btn = document.querySelector('.close-btn')
+
+images.forEach(img => {
+    img.addEventListener('click', () => {
+        images.forEach(i => i.classList.remove('active'))
+        img.classList.add('active')
+        close_btn.classList.add('active')
+    })
+})
+
+if (close_btn) {
+    close_btn.addEventListener('click', () => {
+        images.forEach(i => i.classList.remove('active'))
+        close_btn.classList.remove('active')
+    })
+}
+
 const music = document.querySelector('.music-player audio')
 const music_icon = document.querySelector('.music-icon')
 
 music_icon.addEventListener('click', () => {
     if (music.paused) {
         music.play()
-        music_icon.textContent = '⏹'
+        music_icon.textContent = '⏸'
     } else {
         music.pause()
         music_icon.textContent = '▶'
